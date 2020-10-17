c60bc08e7c97db2304282547163b40dcee134562
João Paulo <joaopaulosantos.21@outlook.com>
2020-10-17 01:30:29 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EzList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao Listador de tarefas!");
+            Console.ReadKey();
+        }
+    }
+}
